969733d6541804e3b30d6372454c08b230a98e26
place222 <place222@hotmail.com>
2017-02-26 23:25:06 +0800

    test

--- src/scripts/detail.js
+++ src/scripts/detail.js
@@ -1,3 +1,25 @@
-(function(){
-    console.log('2312');
-})();
+;(function($){
+    var Beautifier = function(ele,opt){
+        this.$element = ele;
+        this.defaults = {
+            color:'red',
+            fonSize:'12px',
+            textDecoration:'none'
+        };
+        this.options = $.extend({},this.defaults,opt);
+    }
+    Beautifier.prototype = {
+        beautify:function(){
+            return this.$element.css({
+                color:this.options.color,
+                fonSize : this.options.fonSize,
+                textDecoration:this.options.textDecoration
+            })
+        }
+    }
+
+    $.fn.myPlugin = function(options){
+        var beautifier = new Beautifier(this,options);
+        return beautifier.beautify();
+    }
+}($));
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,12 @@
-(function(){
-    $(function(){
-        $("#ta").css('background-color','red');
+$.fn.DataTable = function(options){
+    var defaults = {
+        color:'red',
+        fontSize:'12px'
+    }
+    var settings = $.extend({},defaults,options);
+
+    return this.css({
+        color:settings.color,
+        fontSize:settings.fontSize
     });
-})();
+}
