571a202686c5fb7a7adf252e8a4615e12806e78e
place222 <place222@hotmail.com>
2017-03-03 19:35:01 +0800

    '新的'

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,34 +2,55 @@
     //1.实例方法和静态方法的共享 $.DataTable()
     //2.今天结束 再参考其他同类的看看 如何封装的
      var
-     _initHeader,
      defaults = {
-            id:'#test', //要查找的元素的Id
+            id:'test', //要查找的元素的Id
             color:'red',
             fontSize:'12px',
+            header:[],
             data:[]
     };
 
-    _initHeader = function(){
+    function _initHeader(instance){
+        var html = '<thead>';
+        $.each(instance.options.header,function(i,name){
+            html+= '<th>' + name + '</th>'
+        });
+        html+= '</thead>';
+        instance.$elem.append(html);
+    }
+    function _initContainer(instance){
+        _initRows(instance);
+        _initPagenation(instance);
+    }
+    function _initRows(instance){
+        $.each(instance.options.data,function(i,name){
+            var html = '<tr>';
+            $.each(instance.options.data[i],function(name,prop){
+                html += '<td>' + prop + '</td>'
+            });
+            html+= '</tr>';
+            instance.$elem.append(html);
+        });
+    }
+    function _initPagenation(instance){
+        console.log('插入分页');
+    }
+    function _insertData(data){
 
     }
     //考虑是否使用静态方法为更好
-    function init(table){
-        _initHeader();
-        //1.拿数据 
-        //1.1 直接赋值到data属性的json对象
-        //1.2 是一个函数
-        //1.3
-        _getData();
+    function init(instance){
+        _initHeader(instance);
+        _initContainer(instance);
         //2.解析json结构
         //3.按结构插入表格
     }
-    function Table(options){
+    function Table(elem,options){
         this.options = $.extend({},defaults,options);
-        this.$elem = $(this.options.id);
+        this.$elem = elem ? elem :$('#' + this.options.id);
         init(this);
+        console.log(this);
     };
-
     //API 
     Table.prototype = {
         //考虑文档碎片
@@ -44,8 +65,13 @@
     $.extend({
         DataTable:function(options){
             //看看这里生命周期
-            return new Table(options);
+            return new Table(null,options);
         }
     })
 
+    $.fn.extend({
+        DataTable:function(options){
+            return new Table(this,options);
+        }
+    })
 })(jQuery);
